c2e5ec7bbbc1c14d62c0ac244bf11c4cda186642
Nik O'Brien <niks.programming@gmail.com>
2023-05-03 15:19:28 -0700

    Add validations to users and create first user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    # a user needs to have a username, email and password
+    validates :username, :email, :password, presence: true 
+
+    # a username and email need to be unique
+    validates :username, :email, uniqueness: true 
 end
